8516375b0e65ae8359e3cbb1658a3dba42f9e2c2
andrewb2 <andrewb1@vt.edu>
2017-08-16 11:21:34 -0400

    Update Organizations.vue

--- src/pages/school/Organizations.vue
+++ src/pages/school/Organizations.vue
@@ -6,7 +6,7 @@
     ></v-progress-linear>
     <v-card-title>
       <span class="headline">
-        Organizations
+        Organization Admin
       </span>
       <v-spacer></v-spacer>
       <v-text-field
